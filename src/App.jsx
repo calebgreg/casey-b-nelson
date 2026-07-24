7144b89b0468b06cb4162e45d5ced35c29b077db
calebgreg <caleb@gregcan.com>
2026-07-24 13:38:12 -0400

    Restore dedicated property experiences

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
-import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import AgencyXView from "./components/AgencyXView";
+import CommunityView from "./components/CommunityView";
+import MicedUpView from "./components/MicedUpView";
+import Home, { Header } from "./pages/Home";
+
+function PropertyPage({ children }) {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Header />
+      <main className="property-page">
+        {children(() => navigate("/"))}
+      </main>
+    </>
+  );
+}
 
 export default function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/agency-x" element={<PropertyPage>{(onBack) => <AgencyXView onBack={onBack} />}</PropertyPage>} />
+        <Route path="/miced-up" element={<PropertyPage>{(onBack) => <MicedUpView onBack={onBack} />}</PropertyPage>} />
+        <Route path="/community" element={<PropertyPage>{(onBack) => <CommunityView onBack={onBack} />}</PropertyPage>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
